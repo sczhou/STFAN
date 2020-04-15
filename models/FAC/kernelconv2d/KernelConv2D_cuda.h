22340a90c300f8655dd61b001b3d8a023a4be192
Shangchen Zhou <shangchenzhou@gmail.com>
2020-04-15 10:42:28 +0800

    Format the code.

--- models/FAC/kernelconv2d/KernelConv2D_cuda.h
+++ models/FAC/kernelconv2d/KernelConv2D_cuda.h
@@ -1,14 +1,14 @@
 int KernelConv2D_forward_cuda(
 	at::Tensor& input,
 	at::Tensor& kernel,
-    int kernel_size,
+	int kernel_size,
 	at::Tensor& output
 );
 
 int KernelConv2D_backward_cuda(
 	at::Tensor& input,
 	at::Tensor& kernel,
-    int kernel_size,
+	int kernel_size,
 	at::Tensor& grad_output,
 	at::Tensor& grad_input,
 	at::Tensor& grad_kernel
