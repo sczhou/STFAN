aa601a34bc4b1d5cbe86e63e848d98b038d4a7f3
Shangchen Zhou <shangchenzhou@gmail.com>
2020-10-27 11:22:09 +0800

    Support the latest GPU.

--- models/FAC/kernelconv2d/setup.py
+++ models/FAC/kernelconv2d/setup.py
@@ -10,7 +10,9 @@ nvcc_args = [
     '-gencode', 'arch=compute_50,code=sm_50',
     '-gencode', 'arch=compute_52,code=sm_52',
     '-gencode', 'arch=compute_60,code=sm_60',
-    '-gencode', 'arch=compute_61,code=sm_61'
+    '-gencode', 'arch=compute_61,code=sm_61',
+    '-gencode', 'arch=compute_70,code=sm_70',
+    '-gencode', 'arch=compute_75,code=sm_75'
 ]
 
 setup(
